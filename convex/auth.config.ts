2cb8e216d4d099ce149e20d11d46c001265723e7
Misha <ohmyzhell@gmail.com>
2026-01-24 22:47:27 +0700

    Configure auth: update auth.config.ts, initialize http.ts, revert auth.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,11 +1,8 @@
 export default {
   providers: [
     {
-      domain:
-        process.env.CONVEX_SITE_URL ??
-        process.env.CONVEX_SITE_ORIGIN ??
-        'https://api.klimat22.com',
-      applicationID: 'convex',
+      domain: process.env.CONVEX_SITE_URL,
+      applicationID: "convex",
     },
   ],
-}
+};
